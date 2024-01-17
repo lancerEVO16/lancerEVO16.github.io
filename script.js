fb3b38421ca120c40625e617b958e8c7925eaba8
Lancerotto Samuele <122265832+lancerEVO16@users.noreply.github.com>
2024-01-17 08:29:21 +0100

    bug fix - second commit

--- script.js
+++ script.js
@@ -7,3 +7,65 @@ window.addEventListener('scroll', () => {
         navEl.classList.remove('navbar-scrolled');
     }
 });
+
+function revealLeft() {
+    var reveals = document.querySelectorAll(".reveal-left");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+function revealRight() {
+    var reveals = document.querySelectorAll(".reveal-right");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+function revealTop() {
+    var reveals = document.querySelectorAll(".reveal-top");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+function revealDown() {
+    var reveals = document.querySelectorAll(".reveal-down");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+window.addEventListener("scroll", revealLeft);
+window.addEventListener("scroll", revealRight);
+window.addEventListener("scroll", revealTop);
+window.addEventListener("scroll", revealDown);
+// To check the scroll position on page load
+revealLeft();
+revealRight();
+revealTop();
+revealDown();
